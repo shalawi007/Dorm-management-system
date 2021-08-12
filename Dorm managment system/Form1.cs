0ada9992dabccf0ebc361035e5ed93ce6380bc80
shalawi007 <shoala1994@gmail.com>
2021-08-12 15:56:14 +0300

    test

--- Dorm managment system/Form1.cs
+++ Dorm managment system/Form1.cs
@@ -17,5 +17,14 @@ namespace Dorm_managment_system
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
