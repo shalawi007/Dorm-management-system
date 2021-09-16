0bcfff22c89c1329c604c8f8237eaeb9c546500d
IBRAHIM HAMMADH <TP062501@mail.apu.edu.my>
2021-09-16 19:40:24 +0500

    Terminate forms ( student & warden )

--- Dorm-management-system-master/Dorm managment system/Terminate.Designer.cs
+++ Dorm-management-system-master/Dorm managment system/Terminate.Designer.cs
@@ -30,76 +30,72 @@ namespace Dorm_managment_system
         private void InitializeComponent()
         {
             this.gbTerminate = new System.Windows.Forms.GroupBox();
-            this.listView1 = new System.Windows.Forms.ListView();
             this.lblInfo = new System.Windows.Forms.Label();
-            this.txtStudID = new System.Windows.Forms.TextBox();
-            this.lblStudentID = new System.Windows.Forms.Label();
+            this.txtBookID = new System.Windows.Forms.TextBox();
+            this.lblBookID = new System.Windows.Forms.Label();
             this.btnTerminate = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.btnBack = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.gbTerminate.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // gbTerminate
             // 
             this.gbTerminate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(30)))), ((int)(((byte)(53)))));
-            this.gbTerminate.Controls.Add(this.listView1);
+            this.gbTerminate.Controls.Add(this.dataGridView1);
             this.gbTerminate.Font = new System.Drawing.Font("Sylfaen", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.gbTerminate.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.gbTerminate.Location = new System.Drawing.Point(41, 12);
+            this.gbTerminate.Location = new System.Drawing.Point(31, 44);
+            this.gbTerminate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.gbTerminate.Name = "gbTerminate";
-            this.gbTerminate.Size = new System.Drawing.Size(715, 337);
+            this.gbTerminate.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.gbTerminate.Size = new System.Drawing.Size(817, 449);
             this.gbTerminate.TabIndex = 35;
             this.gbTerminate.TabStop = false;
             this.gbTerminate.Text = "Termination Requests :";
-            // 
-            // listView1
-            // 
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(6, 28);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(683, 288);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
             // lblInfo
             // 
             this.lblInfo.AutoSize = true;
             this.lblInfo.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.lblInfo.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.lblInfo.Location = new System.Drawing.Point(71, 368);
+            this.lblInfo.Location = new System.Drawing.Point(81, 526);
             this.lblInfo.Name = "lblInfo";
-            this.lblInfo.Size = new System.Drawing.Size(349, 22);
+            this.lblInfo.Size = new System.Drawing.Size(441, 26);
             this.lblInfo.TabIndex = 41;
             this.lblInfo.Text = "Upon completing the review please write :";
             // 
-            // txtStudID
+            // txtBookID
             // 
-            this.txtStudID.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.txtStudID.ForeColor = System.Drawing.SystemColors.WindowFrame;
-            this.txtStudID.Location = new System.Drawing.Point(238, 411);
-            this.txtStudID.Name = "txtStudID";
-            this.txtStudID.Size = new System.Drawing.Size(196, 25);
-            this.txtStudID.TabIndex = 40;
-            this.txtStudID.TextChanged += new System.EventHandler(this.txtStudID_TextChanged);
+            this.txtBookID.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtBookID.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.txtBookID.Location = new System.Drawing.Point(272, 583);
+            this.txtBookID.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtBookID.Name = "txtBookID";
+            this.txtBookID.Size = new System.Drawing.Size(223, 29);
+            this.txtBookID.TabIndex = 40;
+            this.txtBookID.TextChanged += new System.EventHandler(this.txtStudID_TextChanged);
             // 
-            // lblStudentID
+            // lblBookID
             // 
-            this.lblStudentID.AutoSize = true;
-            this.lblStudentID.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.lblStudentID.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.lblStudentID.Location = new System.Drawing.Point(109, 411);
-            this.lblStudentID.Name = "lblStudentID";
-            this.lblStudentID.Size = new System.Drawing.Size(123, 22);
-            this.lblStudentID.TabIndex = 39;
-            this.lblStudentID.Text = "1. Student_ID";
+            this.lblBookID.AutoSize = true;
+            this.lblBookID.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblBookID.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.lblBookID.Location = new System.Drawing.Point(125, 583);
+            this.lblBookID.Name = "lblBookID";
+            this.lblBookID.Size = new System.Drawing.Size(143, 26);
+            this.lblBookID.TabIndex = 39;
+            this.lblBookID.Text = "Booking_ID:";
             // 
             // btnTerminate
             // 
             this.btnTerminate.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnTerminate.Location = new System.Drawing.Point(482, 404);
+            this.btnTerminate.Location = new System.Drawing.Point(551, 574);
+            this.btnTerminate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnTerminate.Name = "btnTerminate";
-            this.btnTerminate.Size = new System.Drawing.Size(164, 37);
+            this.btnTerminate.Size = new System.Drawing.Size(187, 49);
             this.btnTerminate.TabIndex = 47;
             this.btnTerminate.Text = "Terminate Contract";
             this.btnTerminate.UseVisualStyleBackColor = true;
@@ -108,9 +104,10 @@ namespace Dorm_managment_system
             // btnExit
             // 
             this.btnExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnExit.Location = new System.Drawing.Point(608, 510);
+            this.btnExit.Location = new System.Drawing.Point(695, 680);
+            this.btnExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnExit.Name = "btnExit";
-            this.btnExit.Size = new System.Drawing.Size(111, 37);
+            this.btnExit.Size = new System.Drawing.Size(127, 49);
             this.btnExit.TabIndex = 46;
             this.btnExit.Text = "Exit";
             this.btnExit.UseVisualStyleBackColor = true;
@@ -119,33 +116,51 @@ namespace Dorm_managment_system
             // btnBack
             // 
             this.btnBack.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnBack.Location = new System.Drawing.Point(466, 510);
+            this.btnBack.Location = new System.Drawing.Point(533, 680);
+            this.btnBack.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnBack.Name = "btnBack";
-            this.btnBack.Size = new System.Drawing.Size(123, 37);
+            this.btnBack.Size = new System.Drawing.Size(141, 49);
             this.btnBack.TabIndex = 45;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 31);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(811, 414);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
             // Terminate
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(781, 577);
+            this.ClientSize = new System.Drawing.Size(893, 769);
             this.Controls.Add(this.btnTerminate);
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.lblInfo);
-            this.Controls.Add(this.txtStudID);
-            this.Controls.Add(this.lblStudentID);
+            this.Controls.Add(this.txtBookID);
+            this.Controls.Add(this.lblBookID);
             this.Controls.Add(this.gbTerminate);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Terminate";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Terminate";
             this.Load += new System.EventHandler(this.Terminate_Load);
             this.gbTerminate.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -155,11 +170,11 @@ namespace Dorm_managment_system
 
         private System.Windows.Forms.GroupBox gbTerminate;
         private System.Windows.Forms.Label lblInfo;
-        private System.Windows.Forms.TextBox txtStudID;
-        private System.Windows.Forms.Label lblStudentID;
+        private System.Windows.Forms.TextBox txtBookID;
+        private System.Windows.Forms.Label lblBookID;
         private System.Windows.Forms.Button btnTerminate;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Button btnBack;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- Dorm-management-system-master/Dorm managment system/Termination of Accomodation.Designer.cs
+++ Dorm-management-system-master/Dorm managment system/Termination of Accomodation.Designer.cs
@@ -1,4 +1,6 @@
 ﻿
+using System;
+
 namespace Dorm_managment_system
 {
     partial class Termination_of_Accomodation
@@ -30,14 +32,14 @@ namespace Dorm_managment_system
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.lblName = new System.Windows.Forms.Label();
+            this.lblBookID = new System.Windows.Forms.Label();
             this.lblStudentID = new System.Windows.Forms.Label();
             this.lblReason = new System.Windows.Forms.Label();
-            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtBookID = new System.Windows.Forms.TextBox();
             this.txtStudentID = new System.Windows.Forms.TextBox();
             this.txtReason = new System.Windows.Forms.TextBox();
-            this.btnSubmit = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -45,29 +47,29 @@ namespace Dorm_managment_system
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.label1.Location = new System.Drawing.Point(21, 19);
+            this.label1.Location = new System.Drawing.Point(24, 25);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(243, 17);
+            this.label1.Size = new System.Drawing.Size(289, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "Accomodation Termination Request";
             // 
-            // lblName
+            // lblBookID
             // 
-            this.lblName.AutoSize = true;
-            this.lblName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(149)))));
-            this.lblName.Location = new System.Drawing.Point(46, 68);
-            this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(67, 15);
-            this.lblName.TabIndex = 1;
-            this.lblName.Text = "Full Name :";
+            this.lblBookID.AutoSize = true;
+            this.lblBookID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(149)))));
+            this.lblBookID.Location = new System.Drawing.Point(53, 91);
+            this.lblBookID.Name = "lblBookID";
+            this.lblBookID.Size = new System.Drawing.Size(90, 20);
+            this.lblBookID.TabIndex = 1;
+            this.lblBookID.Text = "Booking ID :";
             // 
             // lblStudentID
             // 
             this.lblStudentID.AutoSize = true;
             this.lblStudentID.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(149)))));
-            this.lblStudentID.Location = new System.Drawing.Point(46, 102);
+            this.lblStudentID.Location = new System.Drawing.Point(53, 136);
             this.lblStudentID.Name = "lblStudentID";
-            this.lblStudentID.Size = new System.Drawing.Size(68, 15);
+            this.lblStudentID.Size = new System.Drawing.Size(86, 20);
             this.lblStudentID.TabIndex = 2;
             this.lblStudentID.Text = "Student ID :";
             // 
@@ -75,32 +77,30 @@ namespace Dorm_managment_system
             // 
             this.lblReason.AutoSize = true;
             this.lblReason.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(156)))), ((int)(((byte)(149)))));
-            this.lblReason.Location = new System.Drawing.Point(46, 140);
+            this.lblReason.Location = new System.Drawing.Point(53, 187);
             this.lblReason.Name = "lblReason";
-            this.lblReason.Size = new System.Drawing.Size(173, 15);
+            this.lblReason.Size = new System.Drawing.Size(218, 20);
             this.lblReason.TabIndex = 3;
             this.lblReason.Text = "Reason for Termination of stay :";
             // 
-            // txtName
+            // txtBookID
             // 
-            this.txtName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
-            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.txtName.ForeColor = System.Drawing.SystemColors.Window;
-            this.txtName.Location = new System.Drawing.Point(172, 66);
-            this.txtName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(110, 23);
-            this.txtName.TabIndex = 4;
+            this.txtBookID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
+            this.txtBookID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBookID.ForeColor = System.Drawing.SystemColors.Window;
+            this.txtBookID.Location = new System.Drawing.Point(197, 88);
+            this.txtBookID.Name = "txtBookID";
+            this.txtBookID.Size = new System.Drawing.Size(125, 27);
+            this.txtBookID.TabIndex = 4;
             // 
             // txtStudentID
             // 
             this.txtStudentID.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
             this.txtStudentID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtStudentID.ForeColor = System.Drawing.SystemColors.Window;
-            this.txtStudentID.Location = new System.Drawing.Point(172, 100);
-            this.txtStudentID.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtStudentID.Location = new System.Drawing.Point(197, 133);
             this.txtStudentID.Name = "txtStudentID";
-            this.txtStudentID.Size = new System.Drawing.Size(110, 23);
+            this.txtStudentID.Size = new System.Drawing.Size(125, 27);
             this.txtStudentID.TabIndex = 5;
             // 
             // txtReason
@@ -108,12 +108,25 @@ namespace Dorm_managment_system
             this.txtReason.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
             this.txtReason.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.txtReason.ForeColor = System.Drawing.SystemColors.Window;
-            this.txtReason.Location = new System.Drawing.Point(46, 166);
-            this.txtReason.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtReason.Location = new System.Drawing.Point(53, 221);
             this.txtReason.Multiline = true;
             this.txtReason.Name = "txtReason";
-            this.txtReason.Size = new System.Drawing.Size(219, 46);
+            this.txtReason.Size = new System.Drawing.Size(250, 61);
             this.txtReason.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button2.Location = new System.Drawing.Point(355, -3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 29);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "X";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // btnSubmit
             // 
@@ -123,46 +136,30 @@ namespace Dorm_managment_system
             this.btnSubmit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnSubmit.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.btnSubmit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(126)))), ((int)(((byte)(249)))));
-            this.btnSubmit.Location = new System.Drawing.Point(122, 254);
-            this.btnSubmit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnSubmit.Location = new System.Drawing.Point(139, 322);
             this.btnSubmit.Name = "btnSubmit";
-            this.btnSubmit.Size = new System.Drawing.Size(88, 28);
-            this.btnSubmit.TabIndex = 7;
+            this.btnSubmit.Size = new System.Drawing.Size(101, 37);
+            this.btnSubmit.TabIndex = 9;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = false;
-            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
-            // 
-            // button2
-            // 
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.button2.Location = new System.Drawing.Point(311, -2);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(27, 22);
-            this.button2.TabIndex = 8;
-            this.button2.Text = "X";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.button1_Click);
             // 
             // Termination_of_Accomodation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(73)))));
-            this.ClientSize = new System.Drawing.Size(338, 304);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(386, 405);
             this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.txtReason);
             this.Controls.Add(this.txtStudentID);
-            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.txtBookID);
             this.Controls.Add(this.lblReason);
             this.Controls.Add(this.lblStudentID);
-            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblBookID);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.MaximizeBox = false;
             this.Name = "Termination_of_Accomodation";
             this.ShowIcon = false;
@@ -174,16 +171,21 @@ namespace Dorm_managment_system
 
         }
 
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblBookID;
         private System.Windows.Forms.Label lblStudentID;
         private System.Windows.Forms.Label lblReason;
-        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtBookID;
         private System.Windows.Forms.TextBox txtStudentID;
         private System.Windows.Forms.TextBox txtReason;
-        private System.Windows.Forms.Button btnSubmit;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnSubmit;
     }
 }
